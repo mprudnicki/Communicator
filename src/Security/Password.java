54e51d49c44e26fc0b1050960c52129b9fda8d9a
Gerwant <mp.rudnicki@gmail.com>
2016-11-14 17:16:17 +0100

    Simplified introduction to the application

--- src/Security/Password.java
+++ src/Security/Password.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Security;
 import java.math.BigInteger;
 import java.security.spec.InvalidKeySpecException;
